e02870f2d9b5402bd29a5cf7af532fd76bf828f2
m-divya-29 <divya.gracie@gmail.com>
2023-04-22 19:43:07 +0530

    select component by class

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '[app-servers]',
+  selector: '.app-servers',
   template: `<p>Hello world!</p>
             <app-server><app-server/>
             <app-server><app-server/>`,
