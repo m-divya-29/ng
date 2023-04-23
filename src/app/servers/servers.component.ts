bf67311721c0f6d0e9c122a6fc6a768a33e3d2d7
m-divya-29 <divya.gracie@gmail.com>
2023-04-23 21:07:29 +0530

    event binding: display a string on button click

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,9 +6,14 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer = false;
+  serverCreationStatus = 'No server created.';
   
   constructor() {
     //this calls the callback after 2seconds, which sets variable = true
     setTimeout(() => this.allowNewServer = true, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server created successfully!';
   }
 }
