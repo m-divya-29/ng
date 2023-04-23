c0f6280b0cd381185374d6b9643029089ef0f1df
m-divya-29 <divya.gracie@gmail.com>
2023-04-23 20:51:53 +0530

    property binding: add a dynamic button and enable after 2secs of page loading

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '.app-servers',
-  template: `<p>Hello world!</p>
-            <app-server><app-server/>
-            <app-server><app-server/>`,
+  selector: 'app-servers',
+  templateUrl: './servers.component.html'
 })
 export class ServersComponent {
-
+  allowNewServer = false;
+  
+  constructor() {
+    //this calls the callback after 2seconds, which sets variable = true
+    setTimeout(() => this.allowNewServer = true, 2000);
+  }
 }
